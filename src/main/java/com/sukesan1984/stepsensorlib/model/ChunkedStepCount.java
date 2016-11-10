239dd2755bafb810d07f3a15ba5c6daaa197fca3
sukesan1984 <takamikousuke@gmail.com>
2016-11-10 11:19:26 +0900

    add model method

--- src/main/java/com/sukesan1984/stepsensorlib/model/ChunkedStepCount.java
+++ src/main/java/com/sukesan1984/stepsensorlib/model/ChunkedStepCount.java
@@ -1,5 +1,8 @@
 package com.sukesan1984.stepsensorlib.model;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by kosuketakami on 2016/11/07.
  */
@@ -11,5 +14,23 @@ public class ChunkedStepCount {
     public ChunkedStepCount(long unixTimeMillis, int steps) {
         this.unixTimeMillis = unixTimeMillis;
         this.steps = steps;
+    }
+
+    /**
+     * get 'yyyy-mm-dd' format
+     */
+    public String getDayString() {
+        Date date = new Date(this.unixTimeMillis);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        return sdf.format(date);
+    }
+
+    /**
+     * @return unixTimeMillis to hour of start.
+     */
+    public String getHour() {
+        Date date = new Date(this.unixTimeMillis);
+        SimpleDateFormat sdf = new SimpleDateFormat("HH");
+        return sdf.format(date);
     }
 }
