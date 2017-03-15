e412bac71a78abcf2e079415827af1dbe6e246ff
dani <daniel.vila@finc.com>
2017-03-15 17:41:58 +0900

    Catches crashes and logs to Crashlytics
    Catches the crashes reported in crashlytics and logs more information like process id, process name, thread id, etc

--- src/main/java/com/sukesan1984/stepsensorlib/util/Logger.java
+++ src/main/java/com/sukesan1984/stepsensorlib/util/Logger.java
@@ -15,11 +15,15 @@
  */
 package com.sukesan1984.stepsensorlib.util;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.database.Cursor;
 
+import com.crashlytics.android.Crashlytics;
 import com.sukesan1984.stepsensorlib.BuildConfig;
 
 import java.util.Date;
+import java.util.List;
 
 public abstract class Logger {
 
@@ -31,6 +35,40 @@ public abstract class Logger {
         for (StackTraceElement ste : ex.getStackTrace()) {
             log(ste.toString());
         }
+    }
+
+
+    private static String stackToString(StackTraceElement[] stackTrace) {
+        String stack = "Finc-log Stacktrace:";
+        for (StackTraceElement stackTraceElement : stackTrace) {
+            stack += ",at " + (stackTraceElement.toString());
+        }
+        return stack;
+    }
+
+    public static void logInCrashlytics(Context context){
+        Crashlytics.log("Thread id: " + Thread.currentThread().getId());
+        Crashlytics.log("Process id: " + android.os.Process.myPid());
+        Crashlytics.log("Process name: " + getProcessName(context));
+        if (BuildConfig.DEBUG) {
+            Crashlytics.log("Stack: " + stackToString(Thread.currentThread().getStackTrace()));
+        }
+    }
+
+
+    private static String getProcessName(Context context) {
+        int pid = android.os.Process.myPid();
+        ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> infos = manager.getRunningAppProcesses();
+        if (infos != null) {
+            for (ActivityManager.RunningAppProcessInfo processInfo : infos) {
+                if (processInfo.pid == pid) {
+                    return processInfo.processName;
+                }
+            }
+        }
+
+        return null;
     }
 
     public static void log(final Cursor c) {
