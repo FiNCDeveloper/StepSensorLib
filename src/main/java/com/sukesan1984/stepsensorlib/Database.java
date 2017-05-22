853528da176ad4e906831b79a41cf211eef33039
neonankiti <yukinanri0611@gmail.com>
2017-05-22 16:05:29 +0900

    fix compile error

--- src/main/java/com/sukesan1984/stepsensorlib/Database.java
+++ src/main/java/com/sukesan1984/stepsensorlib/Database.java
@@ -292,14 +292,14 @@ public class Database extends SQLiteOpenHelper {
     public int getSteps(final long start, final long end) {
         Cursor c = null;
         try {
-            c = getReadableDatabase();
-            if (c == null) {
-                return 0;
-            }
-            c.query(TABLE_NAME, new String[]{"SUM(" + COLUMN_STEPS + ")"},
+            c = getReadableDatabase()
+                    .query(TABLE_NAME, new String[]{"SUM(" + COLUMN_STEPS + ")"},
                             COLUMN_DATE_AND_HOUR + " >= ? AND " +
                                     COLUMN_DATE_AND_HOUR + " <= ?",
                             new String[]{String.valueOf(start), String.valueOf(end)}, null, null, null);
+            if (c == null) {
+                return 0;
+            }
             int sumSteps;
             if (c.getCount() == 0) {
                 sumSteps = 0;
